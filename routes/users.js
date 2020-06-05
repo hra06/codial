8a28189becd02a374f12e325056511da63012a03
hr06 <h.agarwal06@outlook.com>
2020-06-05 15:49:58 +0530

    Cookie Understanding

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const usersConrtoller = require('../controllers/users_controller');
+const usersController = require('../controllers/users_controller');
 
-router.get('/profile', usersConrtoller.profile);
+router.get('/profile', usersController.profile);
+
+router.get('/sign-in',usersController.SignIn);
+router.get('/sign-up',usersController.signUp);
 
 
 module.exports = router;
