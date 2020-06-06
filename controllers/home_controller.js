17b62e703b296a22c103c5b53123c46783ba56e5
hr06 <h.agarwal06@outlook.com>
2020-06-06 17:31:45 +0530

    fetchinf post from databse to home

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,24 @@
+const Post = require('../models/post')
+
 module.exports.home = function(req, res){
-    return res.render('home', {
-        title: "Home"
-    });
+    
+    // Post.find({},function(err,posts){
+        
+    //     return res.render('home', {
+    //         title: "Codial | Home",
+    //         posts: posts
+    //     });
+    // })
+
+
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home', {
+            title: "Codial | Home",
+            posts: posts
+        });
+    })
+   
+    
 }
 
 // module.exports.actionName = function(req, res){}
