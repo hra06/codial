efc1bed81dc0f1aad60622275393c8484196d973
hra06 <h.agarwal06@outlook.com>
2020-06-07 23:55:23 +0530

    First Async-Await JS

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,7 +3,7 @@ const User = require('../models/user');
 
 
 
-module.exports.home = function(req, res){
+module.exports.home = async function(req, res){
     // console.log(req.cookies);
     // res.cookie('user_id', 25);
 
@@ -15,26 +15,26 @@ module.exports.home = function(req, res){
     // });
 
     // populate the user of each post
-    Post.find({})
-    .populate('user')
-    .populate({
-        path: 'comments',
-        populate: {
-            path: 'user'
-        }
-    })
-    .exec(function(err, posts){
-
-        User.find({}, function(err, users){
-            return res.render('home', {
-                title: "Codeial | Home",
-                posts:  posts,
-                all_users: users
-            });
+    try {
+        let posts = await Post.find({})
+            .populate('user')
+            .populate({
+                path: 'comments',
+                populate: {
+                    path: 'user'
+                }
         });
 
-       
-    })
+        let users = await User.find({});
+
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts:  posts,
+            all_users: users
+        });
+    } catch (err) {
+        console.log('Error',err)        
+    }
 
 }
 
