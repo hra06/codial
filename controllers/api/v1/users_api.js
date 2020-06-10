ceacccb75255090f1d85acf27dfd56a4e17dbd46
hra06 <h.agarwal06@outlook.com>
2020-06-10 16:17:28 +0530

    Authentication Added in API

---
+++ controllers/api/v1/users_api.js
@@ -0,0 +1,28 @@
+const User = require('../../../models/user');
+const jwt = require('jsonwebtoken');
+
+module.exports.createSession = async function(req,res){
+    try {
+        console.log('Hello server')
+        let user = await User.findOne({email:req.body.email}); 
+        console.log(user)  
+        if(!user || user.password != req.body.password){
+            return res.json(422,{
+                message: "Invalid Email or Password"
+            });
+        }
+
+        return res.json(200,{
+            message: "SingIn is Successful here is your token please keep it safe!",
+            data:{
+                token: jwt.sign(user.toJSON(), 'codial', {expiresIn: '10000'})
+            }
+        })
+    } catch (err) {
+        console.log(err);
+        return res.json(500,{
+            message: "Internal Server Error"
+        })
+        
+    }    
+}
