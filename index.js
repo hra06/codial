775d083fd81aff8137265cde3eea072aa13d8c44
hra06 <h.agarwal06@outlook.com>
2020-06-10 15:44:40 +0530

    Api Auth Add Start

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const db = require('./config/mongoose');
 // used for session cookie 
 const session = require('express-session');
 const passport = require('passport');
-const passportLocal = require('./config/passport-local-strategy')
+const passportLocal = require('./config/passport-local-strategy');
+const passportJWT = require('./config/passport-jwt-strategy');
 const MongoStore = require('connect-mongo')(session);
 const sassMiddleware = require('node-sass-middleware');
 const flash = require('connect-flash');
